d0f9750d0a864d04d6f5f99c10318d02a7d48653
Mike Tigas <mike@tig.as>
2016-02-03 12:55:28 -0500

    refactor to use nicknames & store separate fnameprob, lnameprob, and total probability figures.

--- data.js
+++ data.js
@@ -7,59 +7,101 @@ var natural = require('natural')
 var db = require('./prop.json')
 var first = require('./first.json')
 var last = require('./last.json')
+var nicknames = require('./nicknames.json')
 
 /**
  * Constants
  */
 
 var THRESHOLD = 0.87
+var FALLBACK_PROB = 0
 
-var weightedData = db.map(function(p){
-  
-  var bestProb = 0
+
+function scoreFirstName(n) {
+  var fnameProb = FALLBACK_PROB
   var match = -1
-  var name = p.can_nam.split(',')
 
-  if (name.length >= 2) {
-    for (var i = 0; i < first.length; i++) {
-      var fname = name[1].trim().split(' ')
-      fname = fname[0]
-      match = natural.JaroWinklerDistance(fname, first[i].name)
-      if(match > bestProb) {
-        bestProb = match;   
-      }
+  var fname = n.trim().split(' ')
+  fname = fname[0]
+
+  // Check name against common 1990 Census first names, keep the best score.
+  for (var i = 0; i < first.length; i++) {
+    match = natural.JaroWinklerDistance(fname, first[i].name)
+    if(match > fnameProb) {
+      fnameProb = match
     }
+    // Stop if exact match
+    if (fnameProb === 1) break;
+  }
 
-    if (bestProb < THRESHOLD) {
-      return {
-        name: p.can_nam,
-        probability: bestProb,
-        link: p.lin_ima
+  // If we didn't get an exact match, see if nicknames work here.
+  if ((fnameProb < 1) && (!!nicknames[fname])) {
+    for (var j = 0; j < nicknames[fname].length; j++) {
+      var nickname = nicknames[fname][j];
+      for (var i = 0; i < first.length; i++) {
+        match = natural.JaroWinklerDistance(nickname, first[i].name)
+        if(match > fnameProb) {
+          fnameProb = match
+        }
+        // Stop if exact match
+        if (fnameProb === 1) break;
       }
+      // Stop if exact match
+      if (fnameProb === 1) break;
     }
   }
 
-  bestProb = 0
+  return fnameProb
+}
+
+function scoreLastName(n) {
+  var lnameProb = FALLBACK_PROB
   match = -1
-  
-  if(name.length >= 2) {
-   for (var i = 0; i < last.length; i++) {
-      match = natural.JaroWinklerDistance(name[0], last[i].name)
-      if(match > bestProb) {
-        bestProb = match;   
-      }
+
+  var lname = n.trim()
+
+  // Check name against common 1990 Census last names, keep the best score.
+  for (var i = 0; i < last.length; i++) {
+    match = natural.JaroWinklerDistance(lname, last[i].name)
+    if(match > lnameProb) {
+      lnameProb = match;
     }
+    // Stop if exact match
+    if (lnameProb === 1) break;
+  }
+
+  return lnameProb
+}
+
+var weightedData = db.map(function(p){
+
+  var match = -1
+  var name = p.can_nam.split(',')
+
+  var fnameProb = FALLBACK_PROB
+  var lnameProb = FALLBACK_PROB
+
+  if (name.length >= 2) {
+    // Name has more than two comma-delimited parts
+    // (i.e "LASTNAME,FIRSTNAME"), most common case.
+    fnameProb = scoreFirstName(name[1])
+    lnameProb = scoreLastName(name[0])
   } else {
-    bestProb = 1
+    // A single-name string. Like "HIP HOP FOR PRESIDENT".
+
+    // Right now just pass through since firstname and lastname
+    // probabilities should be zero.
   }
 
   return {
     name: p.can_nam,
-    probability: bestProb,
-    link: p.lin_ima
+    fnameProb: fnameProb,
+    lnameProb: lnameProb,
+    probability: (fnameProb+lnameProb)/2.0,
+    link: p.lin_ima,
+    id: p.can_id
   }
 })
-
 
 var funnyNames = weightedData.filter(function(el) {
   return el.probability < THRESHOLD
